107685ed906d1e11974aea0327eef25a637ef217
Will Riley <wjriley61406@icloud.com>
2023-02-19 13:11:29 -0500

    update2

--- game.py
+++ game.py
@@ -2,14 +2,19 @@ import sys, pygame
 pygame.init()
 pygame.font.init()
 
+# File
+with open('scenarioText.txt') as f:
+    scenarioText = f.readlines()
+    
+with open('options.txt') as f:
+    scenarioOptionText = f.readlines()
+
 # Screen Stuff
 size = width, height = 900, 600
 black = 0, 0, 0
 screen = pygame.display.set_mode(size)
 
 # Scenarios
-scenarioText = ["You start the game", "You Played the game", "You Quit"]
-scenarioOptionText = ["Hit 1 To Play or 2 to Quit", "The End", "The End"]
 scenarioOptions = [[1, 2],[0,0],[0,0]]
 
 # Variables
@@ -26,30 +31,43 @@ def decide(key):
 
 
 while True:
+
+    def textWrap(text, text2, tWidth, sHeight):
+        i = 0
+        text = text[:-1]
+        text2 = text2[:-1]
+        while (i < len(text)/tWidth):
+            text_surface1 = font.render(text[(tWidth*i):(tWidth*i+tWidth)], False, (255, 255, 255))
+            rectSize = text_surface1.get_rect()
+            screen.blit(text_surface1, ((width/2)-rectSize.width/2,sHeight+(rectSize.height*i)))
+            i+=1
+        text_surface1 = font.render(text2, False, (255, 255, 180))
+        rectSize = text_surface1.get_rect()
+        screen.blit(text_surface1, ((width/2)-rectSize.width/2,sHeight+(rectSize.height*i)))
+
+
+
+
     # Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_1:
-                scenario = decide(0)
-            elif event.key == pygame.K_2:
-                scenario = decide(1)
-            elif event.key == pygame.K_3:
-                scenario = decide(2)
-            elif event.key == pygame.K_4:
-                scenario = decide(3)
+            try:
+                if event.key == pygame.K_1:
+                    scenario = decide(0)
+                elif event.key == pygame.K_2:
+                    scenario = decide(1)
+                elif event.key == pygame.K_3:
+                    scenario = decide(2)
+                elif event.key == pygame.K_4:
+                    scenario = decide(3)
+            except: print("not an option")
 
     screen.fill(black)
     
     # Draw Text
-    text_surface1 = font.render(scenarioText[scenario], False, (255, 255, 255))
-    text_surface2 = font.render(scenarioOptionText[scenario], False, (255, 255, 255))
-    rectSize = text_surface1.get_rect()
-    rectSize2 = text_surface2.get_rect()
-
-    screen.blit(text_surface1, ((width/2)-rectSize.width/2,4*(height/5)-rectSize.height/2))
-    screen.blit(text_surface2, ((width/2)-rectSize2.width/2,4*(height/5)+rectSize.height/2))
+    textWrap(scenarioText[scenario], scenarioOptionText[scenario], 60, height/2)
 
 
 
