9c8f2ba69c356f8419327d5befdf3e95b823926c
Will Riley <wjriley61406@icloud.com>
2023-02-19 09:45:08 -0500

    screen

--- game.py
+++ game.py
@@ -1,2 +1,15 @@
-import sys, pyGame
-print("Hello World")
+import sys, pygame
+pygame.init()
+
+size = width, height = 900, 600
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+
+    screen.fill(black)
+    pygame.display.flip()
